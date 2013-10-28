962dde8179ccab08d56ad116be42feba9f103eab
Luis Rodriguez <luis.rodriguez@opendeusto.es>
2013-10-28 15:27:42 +0100

    Updated languages which had broken. Updated DEV NOTES to reflect some new observations

--- ENU/boole.cpp
+++ ENU/boole.cpp
@@ -8,118 +8,115 @@
 int _turboFloat;
 //---------------------------------------------------------------------------
 /*ITE*/ /*LCID:00000C0A:00000409*/ /**/
-/*ITE*/ /*DFMFileType*/ /*src\Boole1\NuevoSC.dfm*/
-/*ITE*/ /*DFMFileType*/ /*src\Boole1\app.dfm*/
-/*ITE*/ /*DFMFileType*/ /*src\Boole1\calc.dfm*/
-/*ITE*/ /*DFMFileType*/ /*src\Boole1\uKarnaugh.dfm*/
-/*ITE*/ /*DFMFileType*/ /*src\Boole1\ExpBool.dfm*/
-/*ITE*/ /*DFMFileType*/ /*src\Boole1\FormasN.dfm*/
-/*ITE*/ /*DFMFileType*/ /*src\Boole1\FormSimp.dfm*/
-/*ITE*/ /*DFMFileType*/ /*src\Boole1\Main.dfm*/
-/*ITE*/ /*DFMFileType*/ /*src\Boole1\NandNor.dfm*/
-/*ITE*/ /*DFMFileType*/ /*src\Boole1\NuevoSC.dfm*/
-/*ITE*/ /*DFMFileType*/ /*src\Boole1\SCCompac.dfm*/
-/*ITE*/ /*DFMFileType*/ /*src\Boole1\TVComple.dfm*/
-/*ITE*/ /*DFMFileType*/ /*src\Boole1\TVManual.dfm*/
-/*ITE*/ /*DFMFileType*/ /*src\Boole2\FCalculando.dfm*/
-/*ITE*/ /*DFMFileType*/ /*src\Boole2\ayuda.dfm*/
-/*ITE*/ /*DFMFileType*/ /*src\Boole2\uLog.dfm*/
-/*ITE*/ /*DFMFileType*/ /*src\Boole2\Unit15.dfm*/
-/*ITE*/ /*DFMFileType*/ /*src\Boole2\Unit11.dfm*/
-/*ITE*/ /*DFMFileType*/ /*src\Boole2\Unit12.dfm*/
-/*ITE*/ /*DFMFileType*/ /*src\Boole2\Unit13.dfm*/
-/*ITE*/ /*DFMFileType*/ /*src\Boole2\Unit14.dfm*/
-/*ITE*/ /*DFMFileType*/ /*src\Boole2\Unit10.dfm*/
-/*ITE*/ /*DFMFileType*/ /*src\Boole2\V_Boole2.dfm*/
-/*ITE*/ /*DFMFileType*/ /*src\Boole2\Unit2.dfm*/
-/*ITE*/ /*DFMFileType*/ /*src\Boole2\Unit3.dfm*/
-/*ITE*/ /*DFMFileType*/ /*src\Boole2\Unit4.dfm*/
-/*ITE*/ /*DFMFileType*/ /*src\Boole2\Unit5.dfm*/
-/*ITE*/ /*DFMFileType*/ /*src\Boole2\Unit6.dfm*/
-/*ITE*/ /*DFMFileType*/ /*src\Boole2\Unit8.dfm*/
-/*ITE*/ /*DFMFileType*/ /*src\Boole2\Unit9.dfm*/
-/*ITE*/ /*DFMFileType*/ /*src\Boole2\uSimulacion.dfm*/
-/*ITE*/ /*DFMFileType*/ /*src\Boole2\Unit16.dfm*/
-/*ITE*/ /*DFMFileType*/ /*src\Circuito\V_Imprimir.dfm*/
-/*ITE*/ /*DFMFileType*/ /*src\Circuito\V_Circuito.dfm*/
-/*ITE*/ /*DFMFileType*/ /*src\Comun\uTextoAsoc.dfm*/
-/*ITE*/ /*DFMFileType*/ /*src\Boole1\Weblab.dfm*/
-/*ITE*/ /*RCFileType*/ /*src\res\mensajes.rc*/
-/*ITE*/ /*RCFileType*/ /*exe\boole_DRC.rc*/
+/*ITE*/ /*DFMFileType*/ /*src\\Boole1\\app.dfm*/
+/*ITE*/ /*DFMFileType*/ /*src\\Boole1\\calc.dfm*/
+/*ITE*/ /*DFMFileType*/ /*src\\Boole1\\ExpBool.dfm*/
+/*ITE*/ /*DFMFileType*/ /*src\\Boole1\\FormasN.dfm*/
+/*ITE*/ /*DFMFileType*/ /*src\\Boole1\\FormSimp.dfm*/
+/*ITE*/ /*DFMFileType*/ /*src\\Boole1\\Main.dfm*/
+/*ITE*/ /*DFMFileType*/ /*src\\Boole1\\NandNor.dfm*/
+/*ITE*/ /*DFMFileType*/ /*src\\Boole1\\NuevoSC.dfm*/
+/*ITE*/ /*DFMFileType*/ /*src\\Boole1\\SCCompac.dfm*/
+/*ITE*/ /*DFMFileType*/ /*src\\Boole1\\TVComple.dfm*/
+/*ITE*/ /*DFMFileType*/ /*src\\Boole1\\TVManual.dfm*/
+/*ITE*/ /*DFMFileType*/ /*src\\Boole1\\uKarnaugh.dfm*/
+/*ITE*/ /*DFMFileType*/ /*src\\Boole1\\Weblab.dfm*/
+/*ITE*/ /*DFMFileType*/ /*src\\Boole2\\ayuda.dfm*/
+/*ITE*/ /*DFMFileType*/ /*src\\Boole2\\FCalculando.dfm*/
+/*ITE*/ /*DFMFileType*/ /*src\\Boole2\\uLog.dfm*/
+/*ITE*/ /*DFMFileType*/ /*src\\Boole2\\Unit10.dfm*/
+/*ITE*/ /*DFMFileType*/ /*src\\Boole2\\Unit11.dfm*/
+/*ITE*/ /*DFMFileType*/ /*src\\Boole2\\Unit12.dfm*/
+/*ITE*/ /*DFMFileType*/ /*src\\Boole2\\Unit13.dfm*/
+/*ITE*/ /*DFMFileType*/ /*src\\Boole2\\Unit14.dfm*/
+/*ITE*/ /*DFMFileType*/ /*src\\Boole2\\Unit15.dfm*/
+/*ITE*/ /*DFMFileType*/ /*src\\Boole2\\Unit16.dfm*/
+/*ITE*/ /*DFMFileType*/ /*src\\Boole2\\Unit2.dfm*/
+/*ITE*/ /*DFMFileType*/ /*src\\Boole2\\Unit3.dfm*/
+/*ITE*/ /*DFMFileType*/ /*src\\Boole2\\Unit4.dfm*/
+/*ITE*/ /*DFMFileType*/ /*src\\Boole2\\Unit5.dfm*/
+/*ITE*/ /*DFMFileType*/ /*src\\Boole2\\Unit6.dfm*/
+/*ITE*/ /*DFMFileType*/ /*src\\Boole2\\Unit8.dfm*/
+/*ITE*/ /*DFMFileType*/ /*src\\Boole2\\Unit9.dfm*/
+/*ITE*/ /*DFMFileType*/ /*src\\Boole2\\uSimulacion.dfm*/
+/*ITE*/ /*DFMFileType*/ /*src\\Boole2\\V_Boole2.dfm*/
+/*ITE*/ /*DFMFileType*/ /*src\\Circuito\\V_Circuito.dfm*/
+/*ITE*/ /*DFMFileType*/ /*src\\Circuito\\V_Imprimir.dfm*/
+/*ITE*/ /*DFMFileType*/ /*src\\Comun\\uTextoAsoc.dfm*/
+/*ITE*/ /*RCFileType*/ /*src\\res\\mensajes.rc*/
+/*ITE*/ /*RCFileType*/ /*exe\\boole_DRC.rc*/
 //---------------------------------------------------------------------------
-USEFORMRES("src\Boole1\NuevoSC.dfm", TForm);
-#pragma resource "src\Boole1\NuevoSC.dfm"
-USEFORMRES("src\Boole1\app.dfm", TForm);
-#pragma resource "src\Boole1\app.dfm"
-USEFORMRES("src\Boole1\calc.dfm", TForm);
-#pragma resource "src\Boole1\calc.dfm"
-USEFORMRES("src\Boole1\uKarnaugh.dfm", TForm);
-#pragma resource "src\Boole1\uKarnaugh.dfm"
-USEFORMRES("src\Boole1\ExpBool.dfm", TForm);
-#pragma resource "src\Boole1\ExpBool.dfm"
-USEFORMRES("src\Boole1\FormasN.dfm", TForm);
-#pragma resource "src\Boole1\FormasN.dfm"
-USEFORMRES("src\Boole1\FormSimp.dfm", TForm);
-#pragma resource "src\Boole1\FormSimp.dfm"
-USEFORMRES("src\Boole1\Main.dfm", TForm);
-#pragma resource "src\Boole1\Main.dfm"
-USEFORMRES("src\Boole1\NandNor.dfm", TForm);
-#pragma resource "src\Boole1\NandNor.dfm"
-USEFORMRES("src\Boole1\NuevoSC.dfm", TForm);
-#pragma resource "src\Boole1\NuevoSC.dfm"
-USEFORMRES("src\Boole1\SCCompac.dfm", TForm);
-#pragma resource "src\Boole1\SCCompac.dfm"
-USEFORMRES("src\Boole1\TVComple.dfm", TForm);
-#pragma resource "src\Boole1\TVComple.dfm"
-USEFORMRES("src\Boole1\TVManual.dfm", TForm);
-#pragma resource "src\Boole1\TVManual.dfm"
-USEFORMRES("src\Boole2\FCalculando.dfm", TForm);
-#pragma resource "src\Boole2\FCalculando.dfm"
-USEFORMRES("src\Boole2\ayuda.dfm", TForm);
-#pragma resource "src\Boole2\ayuda.dfm"
-USEFORMRES("src\Boole2\uLog.dfm", TForm);
-#pragma resource "src\Boole2\uLog.dfm"
-USEFORMRES("src\Boole2\Unit15.dfm", TForm);
-#pragma resource "src\Boole2\Unit15.dfm"
-USEFORMRES("src\Boole2\Unit11.dfm", TForm);
-#pragma resource "src\Boole2\Unit11.dfm"
-USEFORMRES("src\Boole2\Unit12.dfm", TForm);
-#pragma resource "src\Boole2\Unit12.dfm"
-USEFORMRES("src\Boole2\Unit13.dfm", TForm);
-#pragma resource "src\Boole2\Unit13.dfm"
-USEFORMRES("src\Boole2\Unit14.dfm", TForm);
-#pragma resource "src\Boole2\Unit14.dfm"
-USEFORMRES("src\Boole2\Unit10.dfm", TForm);
-#pragma resource "src\Boole2\Unit10.dfm"
-USEFORMRES("src\Boole2\V_Boole2.dfm", TForm);
-#pragma resource "src\Boole2\V_Boole2.dfm"
-USEFORMRES("src\Boole2\Unit2.dfm", TForm);
-#pragma resource "src\Boole2\Unit2.dfm"
-USEFORMRES("src\Boole2\Unit3.dfm", TForm);
-#pragma resource "src\Boole2\Unit3.dfm"
-USEFORMRES("src\Boole2\Unit4.dfm", TForm);
-#pragma resource "src\Boole2\Unit4.dfm"
-USEFORMRES("src\Boole2\Unit5.dfm", TForm);
-#pragma resource "src\Boole2\Unit5.dfm"
-USEFORMRES("src\Boole2\Unit6.dfm", TForm);
-#pragma resource "src\Boole2\Unit6.dfm"
-USEFORMRES("src\Boole2\Unit8.dfm", TForm);
-#pragma resource "src\Boole2\Unit8.dfm"
-USEFORMRES("src\Boole2\Unit9.dfm", TForm);
-#pragma resource "src\Boole2\Unit9.dfm"
-USEFORMRES("src\Boole2\uSimulacion.dfm", TForm);
-#pragma resource "src\Boole2\uSimulacion.dfm"
-USEFORMRES("src\Boole2\Unit16.dfm", TForm);
-#pragma resource "src\Boole2\Unit16.dfm"
-USEFORMRES("src\Circuito\V_Imprimir.dfm", TForm);
-#pragma resource "src\Circuito\V_Imprimir.dfm"
-USEFORMRES("src\Circuito\V_Circuito.dfm", TForm);
-#pragma resource "src\Circuito\V_Circuito.dfm"
-USEFORMRES("src\Comun\uTextoAsoc.dfm", TForm);
-#pragma resource "src\Comun\uTextoAsoc.dfm"
-USEFORMRES("src\Boole1\Weblab.dfm", TForm);
-#pragma resource "src\Boole1\Weblab.dfm"
-USERC("src\res\mensajes.rc");
-USERC("exe\boole_DRC.rc");
+USEFORMRES("src\\Boole1\\app.dfm", ap, Form); /* TForm: DesignClass */
+#pragma resource "src\\Boole1\\app.dfm"
+USEFORMRES("src\\Boole1\\calc.dfm", Calculando, Form); /* TForm: DesignClass */
+#pragma resource "src\\Boole1\\calc.dfm"
+USEFORMRES("src\\Boole1\\ExpBool.dfm", ExprBooleana, Form); /* TForm: DesignClass */
+#pragma resource "src\\Boole1\\ExpBool.dfm"
+USEFORMRES("src\\Boole1\\FormasN.dfm", FormasNormales, Form); /* TForm: DesignClass */
+#pragma resource "src\\Boole1\\FormasN.dfm"
+USEFORMRES("src\\Boole1\\FormSimp.dfm", ExpresionSimp, Form); /* TForm: DesignClass */
+#pragma resource "src\\Boole1\\FormSimp.dfm"
+USEFORMRES("src\\Boole1\\Main.dfm", Principal, Form); /* TForm: DesignClass */
+#pragma resource "src\\Boole1\\Main.dfm"
+USEFORMRES("src\\Boole1\\NandNor.dfm", NANDyNOR, Form); /* TForm: DesignClass */
+#pragma resource "src\\Boole1\\NandNor.dfm"
+USEFORMRES("src\\Boole1\\NuevoSC.dfm", SistemaCombinacionalNuevo, Form); /* TForm: DesignClass */
+#pragma resource "src\\Boole1\\NuevoSC.dfm"
+USEFORMRES("src\\Boole1\\SCCompac.dfm", SCFormasCompactas, Form); /* TForm: DesignClass */
+#pragma resource "src\\Boole1\\SCCompac.dfm"
+USEFORMRES("src\\Boole1\\TVComple.dfm", TablaVerdadCompleta, Form); /* TForm: DesignClass */
+#pragma resource "src\\Boole1\\TVComple.dfm"
+USEFORMRES("src\\Boole1\\TVManual.dfm", TablaVerdadManual, Form); /* TForm: DesignClass */
+#pragma resource "src\\Boole1\\TVManual.dfm"
+USEFORMRES("src\\Boole1\\uKarnaugh.dfm", fKarnaugh, Form); /* TForm: DesignClass */
+#pragma resource "src\\Boole1\\uKarnaugh.dfm"
+USEFORMRES("src\\Boole1\\Weblab.dfm", WeblabForm, Form); /* TForm: DesignClass */
+#pragma resource "src\\Boole1\\Weblab.dfm"
+USEFORMRES("src\\Boole2\\ayuda.dfm", fAyuda, Form); /* TForm: DesignClass */
+#pragma resource "src\\Boole2\\ayuda.dfm"
+USEFORMRES("src\\Boole2\\FCalculando.dfm", FormCalculando, Form); /* TForm: DesignClass */
+#pragma resource "src\\Boole2\\FCalculando.dfm"
+USEFORMRES("src\\Boole2\\uLog.dfm", fLog, Form); /* TForm: DesignClass */
+#pragma resource "src\\Boole2\\uLog.dfm"
+USEFORMRES("src\\Boole2\\Unit10.dfm", Form10, Form); /* TForm: DesignClass */
+#pragma resource "src\\Boole2\\Unit10.dfm"
+USEFORMRES("src\\Boole2\\Unit11.dfm", Form11, Form); /* TForm: DesignClass */
+#pragma resource "src\\Boole2\\Unit11.dfm"
+USEFORMRES("src\\Boole2\\Unit12.dfm", Form12, Form); /* TForm: DesignClass */
+#pragma resource "src\\Boole2\\Unit12.dfm"
+USEFORMRES("src\\Boole2\\Unit13.dfm", Form13, Form); /* TForm: DesignClass */
+#pragma resource "src\\Boole2\\Unit13.dfm"
+USEFORMRES("src\\Boole2\\Unit14.dfm", Form14, Form); /* TForm: DesignClass */
+#pragma resource "src\\Boole2\\Unit14.dfm"
+USEFORMRES("src\\Boole2\\Unit15.dfm", Form15, Form); /* TForm: DesignClass */
+#pragma resource "src\\Boole2\\Unit15.dfm"
+USEFORMRES("src\\Boole2\\Unit16.dfm", Form16, Form); /* TForm: DesignClass */
+#pragma resource "src\\Boole2\\Unit16.dfm"
+USEFORMRES("src\\Boole2\\Unit2.dfm", Form2, Form); /* TForm: DesignClass */
+#pragma resource "src\\Boole2\\Unit2.dfm"
+USEFORMRES("src\\Boole2\\Unit3.dfm", Form3, Form); /* TForm: DesignClass */
+#pragma resource "src\\Boole2\\Unit3.dfm"
+USEFORMRES("src\\Boole2\\Unit4.dfm", NewDiagramForm, Form); /* TForm: DesignClass */
+#pragma resource "src\\Boole2\\Unit4.dfm"
+USEFORMRES("src\\Boole2\\Unit5.dfm", Form5, Form); /* TForm: DesignClass */
+#pragma resource "src\\Boole2\\Unit5.dfm"
+USEFORMRES("src\\Boole2\\Unit6.dfm", Form6, Form); /* TForm: DesignClass */
+#pragma resource "src\\Boole2\\Unit6.dfm"
+USEFORMRES("src\\Boole2\\Unit8.dfm", Form8, Form); /* TForm: DesignClass */
+#pragma resource "src\\Boole2\\Unit8.dfm"
+USEFORMRES("src\\Boole2\\Unit9.dfm", Form9, Form); /* TForm: DesignClass */
+#pragma resource "src\\Boole2\\Unit9.dfm"
+USEFORMRES("src\\Boole2\\uSimulacion.dfm", fSimulacion, Form); /* TForm: DesignClass */
+#pragma resource "src\\Boole2\\uSimulacion.dfm"
+USEFORMRES("src\\Boole2\\V_Boole2.dfm", Form1Boole2, Form); /* TForm: DesignClass */
+#pragma resource "src\\Boole2\\V_Boole2.dfm"
+USEFORMRES("src\\Circuito\\V_Circuito.dfm", FormR, Form); /* TForm: DesignClass */
+#pragma resource "src\\Circuito\\V_Circuito.dfm"
+USEFORMRES("src\\Circuito\\V_Imprimir.dfm", FormI, Form); /* TForm: DesignClass */
+#pragma resource "src\\Circuito\\V_Imprimir.dfm"
+USEFORMRES("src\\Comun\\uTextoAsoc.dfm", fTextoAsoc, Form); /* TForm: DesignClass */
+#pragma resource "src\\Comun\\uTextoAsoc.dfm"
+USERC("src\\res\\mensajes.rc");
+USERC("exe\\boole_DRC.rc");
 //---------------------------------------------------------------------------
 #define DllEntryPoint
